17928ffe8a6b5c45233f339a7e0a620e52e1240d
alina <valinurovaarr@gmail.com>
2023-11-30 22:22:00 +0300

    v1

--- main.tf
+++ main.tf
@@ -8,18 +8,18 @@ terraform {
 }
 
 provider "yandex" {
-service_account_key_file = "key.json"
-  cloud_id = "b1g71e95h51okii30p25"
-  folder_id = "b1gqqiji145iks917iem"
+  service_account_key_file = var.service_account_key_file
+  cloud_id = var.cloud_id
+  folder_id = var.folder_id
   zone = "ru-central1-a"
 }
 
 resource "yandex_iam_service_account" "sa" {
-  name = "sa-task1"
+  name = "vvot03-sa-task1"
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "sa-admin" {
-  folder_id = "b1gqqiji145iks917iem"
+  folder_id = var.folder_id
   role      = "admin"
   member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
 }
@@ -45,7 +45,7 @@ resource "yandex_function" "vvot03-face-detection" {
   execution_timeout  = "10"
   service_account_id = yandex_iam_service_account.sa.id
   content {
-    zip_filename = "func1.zip"
+    zip_filename = "face-detection.zip"
   }
   
   environment = {
@@ -109,10 +109,19 @@ column {
       type = "Utf8"
       not_null = true
     }
-    column {
+column {
       name = "cut_key"
       type = "Utf8"
       not_null = true
+    }
+column {
+        name = "has_name"
+        type = "Bool"
+    }
+
+column {
+        name = "name"
+        type = "string"
     }
 
   primary_key = ["original_key","cut_key"]
@@ -129,7 +138,7 @@ resource "yandex_function" "vvot03-face-cut" {
   execution_timeout  = "50"
   service_account_id = yandex_iam_service_account.sa.id
   content {
-    zip_filename = "func22.zip"
+    zip_filename = "face-cut.zip"
   }
   
   environment = {
@@ -137,6 +146,8 @@ resource "yandex_function" "vvot03-face-cut" {
     AWS_SECRET_ACCESS_KEY = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
     AWS_DEFAULT_REGION = "ru-central1-a"
     DATABASE_URL = yandex_ydb_database_serverless.vvot03-db-photo-face.ydb_full_endpoint
+    FACE_STORAGE = var.bucket_faces
+    PHOTO_STORAGE = var.bucket_photo
   }
 }
 
@@ -169,3 +180,94 @@ resource "yandex_storage_bucket" "vvot03-faces" {
 	secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
 	bucket = "vvot03-faces"
 }
+
+resource "yandex_api_gateway" "vvot03-apigw" {
+  name = "vvot03-apigw"
+  description = "api gateway for faces"
+  labels = {
+    label       = "label"
+    empty-label = ""
+  }
+  spec = <<-EOT
+openapi: 3.0.0
+info:
+  title: vvot03-apigw
+  version: 1.0.0
+paths:
+  /:
+    get:
+      parameters:
+      - name: face
+        in: query
+        description: 'key of object in object storage'
+        required: true
+        schema:
+          type: string
+      responses:
+        '200':
+          description: OK
+          content:
+              image/png:
+                schema: 
+                  type: string
+                  format: binary
+      x-yc-apigateway-integration:
+        type: object_storage
+        bucket: ${var.bucket_faces}
+        object: '{face}'
+        presigned_redirect: false
+        service_account_id: ${yandex_iam_service_account.sa.id}
+        operationId: static
+        responses:
+        '200':
+          description: OK
+          content:
+              image/png:
+                schema: 
+                  type: string
+                  format: binary
+EOT
+}
+
+resource "yandex_function" "vvot03-boot" {
+  name               = "vvot03-boot"
+  description        = "Обработчик для бота"
+  user_hash          = "any_user_defined_string1"
+  runtime            = "python311"
+  entrypoint         = "bot.handler"
+  memory             = "128"
+  execution_timeout  = "10"
+  service_account_id = yandex_iam_service_account.sa.id
+  tags               = ["my_tag"]
+  content {
+    zip_filename = "3.zip"
+  }
+  environment = {
+    APITG = yandex_api_gateway.vvot03-apigw.id
+    TGKEY = var.tgkey
+    DATABASE_URL = yandex_ydb_database_serverless.vvot03-db-photo-face.ydb_full_endpoint
+    AWS_ACCESS_KEY_ID = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+    AWS_SECRET_ACCESS_KEY = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+    AWS_DEFAULT_REGION = "ru-central1-a"
+    FACE_STORAGE = var.bucket_faces
+    PHOTO_STORAGE = var.bucket_photo
+  }
+}
+
+resource "yandex_function_iam_binding" "vvot03-boot-iam" {
+  function_id = yandex_function.vvot03-boot.id
+  role        = "serverless.functions.invoker"
+
+  members = [
+    "system:allUsers",
+  ]
+}
+
+output "bot_id" {
+  value = yandex_function.vvot03-boot.id
+}
+
+data "http" "webhook" {
+  url = "https://api.telegram.org/bot6761917445:AAFhQOcud0sTuDwgYb4lk0tXzFPvbnrq7t8/setWebhook?url=https://functions.yandexcloud.net/${yandex_function.vvot03-boot.id}"
+}
+
